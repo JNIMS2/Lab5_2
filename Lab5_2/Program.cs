81515b54782fbeecb57e8d0e42fda6b6bcce6d88
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-08-10 16:27:21 -0400

    some stuff

--- Lab5_2/Program.cs
+++ Lab5_2/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Lab5_2
 {
+    //create an enum called CarMake
+    enum CarMake
+    {
+    Ford,
+    Chevrolet,
+    Chrysler,
+    Honda,
+    Toyota
+    }
+
+    class Car
+    {//make these public until tested. then private...
+        public CarMake Make;//fine
+        public string Model;
+        public int Year;
+        public decimal Price;
+
+        public Car(CarMake _Make, string _Model, int _Year, decimal _Price )
+        {
+            Make = _Make;
+            Model = _Model;
+            Year = _Year;
+            Price = _Price;
+        } 
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ctest");
+            //Car testcar = new Car();
+            //testcar.Make = CarMake.Chevrolet;
+            //Console.WriteLine(testcar.Make);
+            
         }
     }
 }
